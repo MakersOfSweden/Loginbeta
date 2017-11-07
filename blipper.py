aef1980f7d117085c4a2e2d8bc945dfc2d051142
Johnny Karhinen <johnny@nurrd.se>
2017-11-08 00:39:57 +0100

    Break out functions wait_for_valid_rfid(), add_user(), log_action() and trigger_random_sound()

--- blipper.py
+++ blipper.py
@@ -7,7 +7,8 @@ import time
 import requests
 import hasher
 import getpass
-while True:
+
+def wait_for_valid_rfid():
     while True:
         try:
             temp = getpass.getpass("Blip me! ")
@@ -17,6 +18,31 @@ while True:
             print("Blip not recognised")
     print("")
 
+    return rfId
+
+
+def add_user(nick, rfid):
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO People VALUES (?,?,?,?,?);", (rfId, nick_temp, 1, 0, time.time()))
+    return cursor.lastrowid
+
+
+def log_action(who, what):
+    requests.put('http://127.0.0.1:5001/', json = {'who': who, 'what': what})
+
+
+def trigger_random_sound():
+    try:
+        requests.get('http://192.168.42.12:5000/',timeout=0.001)
+    except:
+        pass
+
+
+
+
+while True:
+    rfid = wait_for_valid_rfid()
+
     connection = lite.connect('People.db')
     connect.row_factory = lite.Row
 
@@ -25,19 +51,18 @@ while True:
         cur.execute("SELECT * FROM People WHERE blipId = ?", (rfId,))
         data = cur.fetchone()
 
-        if data is None:  # there is no user with this ID tag
+        if not data:  # there is no user with this ID tag
             print("-----------------------------------------------")
             print('There is no rfidtag named ', rfId, ' creating instance!')
 
             nick_temp = input("input your nick: ")
 
-            cur.execute("INSERT INTO People VALUES (?,?,?,?,?);",
-                        (rfId, nick_temp, 1, 0, time.time()))
-            requests.put('http://127.0.0.1:5001/', json = {'who': nick_temp, 'what': "login"})
-            try:
-                requests.get('http://192.168.42.12:5000/',timeout=0.001)
-            except:
-                print(" ")
+            add_user(nick_temp, rfId)
+
+            log_action(nick_temp, 'login')
+
+            trigger_random_sound()
+
             print('you now exist and are logged in! dont forget to logout!')
             print("-----------------------------------------------")
         else:           # there is user with this ID tag
@@ -46,11 +71,11 @@ while True:
                 new_total_time = time_spent + data['totalTime']
                 cur.execute("UPDATE People SET totalTime=?, isHere=? WHERE blipId=?",
                             (new_total_time, 0, rfId))
-                requests.put('http://127.0.0.1:5001/', json = {'who': str(data['Nick']), 'what': "logout"})
-                try:
-                    requests.get('http://192.168.42.12:5000/',timeout=0.001)
-                except:
-                    print(" ")
+
+                log_action(data['Nick'], 'logout')
+
+                trigger_random_sound()
+
                 print("-----------------------------------------------")
                 print("Goodbye " + str(data[2]) + " your highscore is: " +
                       str(new_total_time))
@@ -59,11 +84,11 @@ while True:
             else:   # is not logged in => log hen in
                 cur.execute("UPDATE People SET lastLogin=?, isHere=? WHERE blipId=?",
                             (time.time(), 1, rfId))
-                requests.put('http://127.0.0.1:5001/', json = {'who': str(data['Nick']), 'what': "login"})
-                try:
-                    requests.get('http://192.168.42.12:5000/',timeout=0.001)
-                except:
-                    print(" ")
+
+                log_action(data['Nick'], 'login')
+
+                trigger_random_sound()
+
                 print("-----------------------------------------------")
                 print("Welcome " + str(data['Nick']))
                 print("-----------------------------------------------")
