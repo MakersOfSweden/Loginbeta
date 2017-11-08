9a991526291c7ef7918172a990f0ba4e7b5e4878
Johnny Karhinen <johnny@nurrd.se>
2017-11-08 01:41:08 +0100

    Indentation fix in lights.py

--- lights.py
+++ lights.py
@@ -5,11 +5,11 @@ def on():
     try:
         requests.get('http://192.168.42.10:5000/light/on', timeout=0.1)
     except (requests.exceptions.Timeout, requests.ConnectionError):
-            pass
+        pass
 
 
 def off():
     try:
         requests.get('http://192.168.42.10:5000/light/off', timeout=0.1)
     except (requests.exceptions.Timeout, requests.ConnectionError):
-            pass
+        pass
