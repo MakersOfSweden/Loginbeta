61a1848aa29f386bbf3f0f3a12971623c4e6f5f3
Lars Lindholm <larlin@lysator.liu.se>
2018-04-01 16:35:48 +0200

    * Fixed a faulty sql command. * Added commit() to all user commands that change data to commit changes   to disk.

--- users.py
+++ users.py
@@ -1,4 +1,5 @@
 import sqlite3
+import time
 
 _connection = sqlite3.connect('People.db')
 _connection.row_factory = sqlite3.Row
@@ -6,18 +7,21 @@ _connection.row_factory = sqlite3.Row
 
 def add(nick, rfid):
     cursor = _connection.cursor()
-    cursor.execute("INSERT INTO People VALUES (?,?,?,?,?);", (rfid, nick_temp, 1, 0, time.time()))
+    cursor.execute("INSERT INTO People (blipId, Nick, isHere, totalTime, lastLogin) VALUES (?,?,?,?,?);", (rfid, nick, 1, 0, time.time()))
+    _connection.commit()
     return cursor.lastrowid
 
 
 def logout(rfid):
     cursor = _connection.cursor()
     cursor.execute('UPDATE People SET totalTime=totalTime + (strftime("%s", "now") - lastLogin), isHere=0 WHERE blipId=?', [rfid])
+    _connection.commit()
 
 
 def login(rfid):
     cursor = _connection.cursor()
     cursor.execute('UPDATE People SET lastLogin=strftime("%s", "now"), isHere=1 WHERE blipId=?', [rfid])
+    _connection.commit()
 
 
 def fetch(rfid):
@@ -41,4 +45,5 @@ def highscore():
 def logout_all():
     cursor = _connection.cursor()
     cursor.execute("UPDATE People SET isHere = 0")
+    _connection.commit()
 
